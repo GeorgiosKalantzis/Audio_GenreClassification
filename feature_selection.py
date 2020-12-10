4e5d5210d7cce374654db9ca0921553b19bb8baa
sotirismos <sotiriosmos@gmail.com>
2020-12-10 18:41:34 +0200

    Added 1 more filter methods. Added 3 wrapper methods.

--- feature_selection.py
+++ feature_selection.py
@@ -74,10 +74,7 @@ duplicated_columns = train_features_T[train_features_T.duplicated()].index.value
 Correlated filter methods.
 Features should be highly corellated with the target.
 Features should be uncorellated among themselves.
-We will use the Pearson corellation(default).
 """
-
-#####**** kendall or spearman ****#####
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -86,7 +83,7 @@ import matplotlib.pyplot as plt
 corr_features = set()
 
 #Create the correlation matrix
-corr_matrix = X_train.corr() #pearson, kendall or spearman
+corr_matrix = X_train.corr(method='spearman') #pearson, kendall or spearman
 
 #display a heatmap of the correlation matrix
 plt.figure(figsize=(18,8))
@@ -95,17 +92,13 @@ sns.heatmap(corr_matrix)
 
 for i in range(len(corr_matrix.columns)):
     for j in range(i):
-        if abs(corr_matrix.iloc[i,j]) > 0.9 and (i != j):
+        if(corr_matrix.iloc[i,j]) > 0.9 and (i != j):
+            colname = corr_matrix.columns[i]
+            corr_features.add(colname)
 
-            colname1 = corr_matrix.columns[i]
-            colname2 = corr_matrix.columns[j]
-
-            corr_features.add(colname1)
-
-            corr_features.add(colname2)
-
-# Correlation result
-# corr_features = {'mfccs_2_mean', 'spectral_centroid_mean', 'spectral_rolloff_mean'}
+#drop highly corellated features
+#X_train.drop(labels=corr_features, axis=1, inplace=True)
+#X_test.drop(labels=corr_features, axis=1, inplace=True)
 
 
 """
@@ -133,8 +126,136 @@ select_k = 30 #whatever we want
 # create the SelectKBest with the mutual info(info gain) strategy.
 selection = SelectKBest(mutual_info_classif, k=select_k).fit(X_train, y_train)
 
+#plot the scores
+plt.bar([i for i in range(len(selection.scores_))], selection.scores_)
+plt.show()
+
 # display the retained features.
 features = X_train.columns[selection.get_support()]
 print(features)
 
 ###*** We have to drop the other features(with less info gain from X_train,X_test) ***###
+
+"""
+Feature importance based on ROC-AUC metrics.
+AUC–ROC curve is used in bi–multi class classification problems.
+"""
+
+# import the DecisionTree Algorithm and evaluation score.
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
+
+# list of the resulting scores.
+roc_values = []
+
+# loop over all features and calculate the score.
+for feature in x_train.columns:
+    clf = DecisionTreeClassifier()
+    clf.fit(x_train[feature].to_frame(), y_train)
+    y_scored = clf.predict_proba(x_test[feature].to_frame())
+    roc_values.append(roc_auc_score(y_test, y_scored[:, 1]))
+
+# create a Pandas Series for visualisation.
+roc_values = pd.Series(roc_values)
+roc_values.index = X_train.columns
+
+# show the results.
+print(roc_values.sort_values(ascending=False))
+
+###*** We have to drop the other features(with less info gain from X_train,X_test) ***###
+
+"""
+WRAPPER METHODS.
+Wrapper methods work by evaluating a subset of features using a machine learning algorithm
+that employs a search strategy to look through the space of possible feature subsets,
+evaluating each subset based on the quality of the performance of a given algorithm.
+"""
+"""
+FORWARD FEATURE SELECTION.
+an iterative method in which we start by evaluating all features individually,
+and then select the one that results in the best performance.
+"""
+
+from mlxtend.feature_selection import SequentialFeatureSelector
+
+# import the algorithm you want to evaluate on your features.
+from sklearn.ensemble import RandomForestClassifier
+
+# create the SequentialFeatureSelector object, and configure the parameters.
+sfs = SequentialFeatureSelector(RandomForestClassifier(),
+           k_features=10,
+           forward=True,
+           scoring='accuracy',
+           cv=2)
+
+# fit the object to the training data.
+sfs = sfs.fit(X_train, y_train)
+
+# print the selected features.
+selected_features = X_train.columns[list(sfs.k_feature_idx_)]
+print('selected features from forward selection:', selected_features)
+
+# print the final prediction score.
+print('accuracy:', sfs.k_score_)
+
+# transform to the newly selected features.
+#X_train = sfs.transform(X_train)
+#X_test = sfs.transform(X_test)
+
+"""
+BACKWARD FEATURE ELIMINATION.
+At each iteration, backward feature elimination removes one feature at a time.
+This feature can be also described as the least significant feature among
+the remaining available ones.
+"""
+
+# just set forward=False for backward feature selection.
+# create theSequentialFeatureSelector object, and configure the parameters.
+sbs = SequentialFeatureSelector(RandomForestClassifier(),
+           k_features=10,
+           forward=False,
+           scoring='accuracy',
+           cv=2)
+
+# fit the object to our training data.
+sbs = sbs.fit(X_train, y_train)
+
+# print the selected features.
+selected_features = X_train.columns[list(sbs.k_feature_idx_)]
+print('selected features from backward selection:', selected_features)
+
+# print the final prediction score.
+print('accuracy:', sbs.k_score_)
+
+# transform to the newly selected features.
+#X_train = sbs.transform(X_train)
+#X_test = sbs.transform(X_test)
+
+"""
+EXHAUSTIVE FEATURE SELECTION.
+This method searches across all possible feature combinations.
+Its aim is to find the best performing feature subset.
+"""
+# import the algorithm you want to evaluate on your features.
+from mlxtend.feature_selection import ExhaustiveFeatureSelector
+
+# create the ExhaustiveFeatureSelector object.
+efs = ExhaustiveFeatureSelector(RandomForestClassifier(),
+           min_features=4,
+           max_features=10,
+           scoring='accuracy',
+           cv=2)
+
+# fit the object to the training data.
+efs = efs.fit(X_train, y_train)
+
+# print the selected features.
+selected_features = X_train.columns[list(efs.k_feature_idx_)]
+print('selected features from exhaustive selection:', selected_features)
+
+# print the final prediction score.
+print('accuracy:', efs.k_score_)
+
+# transform to the newly selected features.
+#X_train = efs.transform(X_train)
+#X_test = efs.transform(X_test)
