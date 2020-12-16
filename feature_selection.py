c7641685ae8a1d1ff762548a3249124379fc4542
GeorgiosKalantzis <georgekalantzis997@gmail.com>
2020-12-16 19:17:07 +0200

    Fix some mistakes

--- feature_selection.py
+++ feature_selection.py
@@ -1,79 +1,109 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec  2 19:36:33 2020
-
-@author: Sotiris
-"""
-
-
-"""
-Correlated filter methods.
-Features should be highly corellated with the target.
-Features should be uncorellated among themselves.
-We will use the Pearson corellation(default).
-"""
-
-##### kendall or spearman !!!! #####
-
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt 
 from sklearn.model_selection import train_test_split
-
-train = pd.read_csv('df_features.csv', header=0)
-y = train.iloc[0:,0]
-x = train.iloc[0:,1:]
-
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.10, random_state = 2020, stratify=y)
-
-#Create a set to hold the correlated features
-corr_features = set()
-
-#Create the correlation matrix
-corr_matrix = X_train.corr() #pearson, kendall or spearman
-
-#display a heatmap of the correlation matrix
-plt.figure(figsize=(18,8))
-sns.heatmap(corr_matrix)
+import numpy as np
+data = pd.read_csv('df_features.csv', header=None)
+y = data.iloc[1:,0]
+x = data.iloc[1:,1:]
 
 
-for i in range(len(corr_matrix.columns)):
-    for j in range(i):
-        
-        if abs(corr_matrix.iloc[i,j]) > 0.9 and (i != j):  
-            
-            colname1 = corr_matrix.columns[i]
-            colname2 = corr_matrix.columns[j]
-           
-            corr_features.add(colname1)
-            
-            corr_features.add(colname2)
+"""
 
-# Correlation result            
-# corr_features = {'mfccs_2_mean', 'spectral_centroid_mean', 'spectral_rolloff_mean'}
-            
+"""
+"""
+WRAPPER METHODS.
+Wrapper methods work by evaluating a subset of features using a machine learning algorithm
+that employs a search strategy to look through the space of possible feature subsets,
+evaluating each subset based on the quality of the performance of a given algorithm.
+"""
 
 
+"""
+EXHAUSTIVE FEATURE SELECTION.
+This method searches across all possible feature combinations.
+Its aim is to find the best performing feature subset.
+"""
+# import the algorithm you want to evaluate on your features.
+from mlxtend.feature_selection import ExhaustiveFeatureSelector
+from sklearn.ensemble import RandomForestClassifier
 
+# create the ExhaustiveFeatureSelector object.
+efs = ExhaustiveFeatureSelector(RandomForestClassifier(),
+           min_features=45,
+           max_features=70,
+           scoring='accuracy',
+           cv=2)
 
+# fit the object to the training data.
+efs = efs.fit(x, y)
 
+# print the selected features.
+selected_features1 = x.columns[list(efs.k_feature_idx_)]
+print('selected features from exhaustive selection:', selected_features1)
 
+# print the final prediction score.
+print('accuracy:', efs.k_score_)
 
+# transform to the newly selected features.
+#X_train = efs.transform(X_train)
+#X_test = efs.transform(X_test)
 
+"""
+FORWARD FEATURE SELECTION.
+an iterative method in which we start by evaluating all features individually,
+and then select the one that results in the best performance.
+"""
 
+from mlxtend.feature_selection import SequentialFeatureSelector
 
+# import the algorithm you want to evaluate on your features.
+from sklearn.ensemble import RandomForestClassifier
 
+# create the SequentialFeatureSelector object, and configure the parameters.
+sfs = SequentialFeatureSelector(RandomForestClassifier(),
+           k_features=60,
+           forward=True,
+           scoring='accuracy',
+           cv=2)
 
+# fit the object to the training data.
+sfs = sfs.fit(x, y)
 
+# print the selected features.
+selected_features2 = x.columns[list(sfs.k_feature_idx_)]
+print('selected features from forward selection:', selected_features2)
 
+# print the final prediction score.
+print('accuracy:', sfs.k_score_)
 
+# transform to the newly selected features.
+#X_train = sfs.transform(X_train)
+#X_test = sfs.transform(X_test)
 
+"""
+BACKWARD FEATURE ELIMINATION.
+At each iteration, backward feature elimination removes one feature at a time.
+This feature can be also described as the least significant feature among
+the remaining available ones.
+"""
 
+# just set forward=False for backward feature selection.
+# create theSequentialFeatureSelector object, and configure the parameters.
+sbs = SequentialFeatureSelector(RandomForestClassifier(),
+           k_features=60,
+           forward=False,
+           scoring='accuracy',
+           cv=100)
 
+# fit the object to our training data.
+sbs = sbs.fit(x, y)
 
+# print the selected features.
+selected_features3 = x.columns[list(sbs.k_feature_idx_)]
+print('selected features from backward selection:', selected_features3)
 
+# print the final prediction score.
+print('accuracy:', sbs.k_score_)
 
-
-
-
+# transform to the newly selected features.
+#X_train = sbs.transform(X_train)
+#X_test = sbs.transform(X_test)
